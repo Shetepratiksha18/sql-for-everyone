4a8c6ed6b779f594bb327d4447555c42443d38fc
Shetepratiksha18 <146553547+Shetepratiksha18@users.noreply.github.com>
2025-07-10 22:28:20 +0530

    insert_values.sql

---
+++ 01_basic_commands/01_basic_commands/insert_values.sql
@@ -0,0 +1,7 @@
+-- Inserting values into Students table
+INSERT INTO Students (id, name, age)
+VALUES (1, 'Pratiksha', 21),
+       (2, 'Amit', 22),
+       (3,'Pritesh',22),
+       (4,'Sakshi',23);
+
